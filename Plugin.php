8329c3aa7c36076e5105b611545bac788079711c
Eugene Croquette <croqo@outlook.com>
2021-06-20 02:47:47 +0300

    init scaffold

---
+++ Plugin.php
@@ -0,0 +1,96 @@
+<?php namespace Croqo\Motion;
+
+use Backend;
+use System\Classes\PluginBase;
+
+/**
+ * Motion Plugin Information File
+ */
+class Plugin extends PluginBase
+{
+    /**
+     * Returns information about this plugin.
+     *
+     * @return array
+     */
+    public function pluginDetails()
+    {
+        return [
+            'name'        => 'Motion',
+            'description' => 'No description provided yet...',
+            'author'      => 'Croqo',
+            'icon'        => 'icon-leaf'
+        ];
+    }
+
+    /**
+     * Register method, called when the plugin is first registered.
+     *
+     * @return void
+     */
+    public function register()
+    {
+
+    }
+
+    /**
+     * Boot method, called right before the request route.
+     *
+     * @return array
+     */
+    public function boot()
+    {
+
+    }
+
+    /**
+     * Registers any front-end components implemented in this plugin.
+     *
+     * @return array
+     */
+    public function registerComponents()
+    {
+        return []; // Remove this line to activate
+
+        return [
+            'Croqo\Motion\Components\MyComponent' => 'myComponent',
+        ];
+    }
+
+    /**
+     * Registers any back-end permissions used by this plugin.
+     *
+     * @return array
+     */
+    public function registerPermissions()
+    {
+        return []; // Remove this line to activate
+
+        return [
+            'croqo.motion.some_permission' => [
+                'tab' => 'Motion',
+                'label' => 'Some permission'
+            ],
+        ];
+    }
+
+    /**
+     * Registers back-end navigation items for this plugin.
+     *
+     * @return array
+     */
+    public function registerNavigation()
+    {
+        return []; // Remove this line to activate
+
+        return [
+            'motion' => [
+                'label'       => 'Motion',
+                'url'         => Backend::url('croqo/motion/mycontroller'),
+                'icon'        => 'icon-leaf',
+                'permissions' => ['croqo.motion.*'],
+                'order'       => 500,
+            ],
+        ];
+    }
+}
---
+++ components/Player.php
@@ -0,0 +1,22 @@
+<?php namespace Croqo\Motion\Components;
+
+use Cms\Classes\ComponentBase;
+
+/**
+ * Player Component
+ */
+class Player extends ComponentBase
+{
+    public function componentDetails()
+    {
+        return [
+            'name' => 'Player Component',
+            'description' => 'No description provided yet...'
+        ];
+    }
+
+    public function defineProperties()
+    {
+        return [];
+    }
+}
