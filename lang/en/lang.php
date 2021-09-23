c28d8beac1cf6b4f5225c8c26f3bb3cbbde90160
Eugene Croquette <eugene@croqo.co>
2021-09-23 14:28:20 +0300

    Multilanguage support

---
+++ lang/en/lang.php
@@ -0,0 +1,13 @@
+<?php return [
+    "plugin" => [
+        "name" => "Lottie",
+        "description" => "Adds awesome Lottie animations to your OctoberCMS website with ease"
+    ],
+    "components" => [
+        "player" => 
+        [
+            "name" => "Lottie animation",
+            "description" => "JS player / animation container"
+        ],
+    ],
+];
